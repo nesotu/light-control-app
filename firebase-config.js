afad088903efe0bbde2d89c24e47c5bbb2a7e0ec
nesotu <h.kenshin1227@icloud.com>
2025-08-09 17:16:59 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAkBfg8b3Dp-Ee63ntqD53X45Br5qZ9Is",
+  authDomain: "right-c4ec8.firebaseapp.com",
+  databaseURL: "https://right-c4ec8-default-rtdb.firebaseio.com/",
+  projectId: "right-c4ec8",
+  storageBucket: "right-c4ec8.firebasestorage.app",
+  messagingSenderId: "905447571238",
+  appId: "1:905447571238:web:2760c94e4a0aa0fe858be7",
+  measurementId: "G-Y5NK9E1E96"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
